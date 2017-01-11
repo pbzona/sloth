d100b1005f6767a521196f5eb5d0a3cee0266c68
Phil Zona <phil.b.zona@gmail.com>
2017-01-11 17:42:03 -0500

    Initial commit...fingers crossed on the bash script :

---
+++ .vimrc
@@ -0,0 +1,46 @@
+" Using these plugins:
+" Pathogen https://github.com/tpope/vim-pathogen
+" Enhanced JS syntax https://github.com/pangloss/vim-javascript
+" Luna-term syntax https://github.com/notpratheek/vim-luna
+" Airline https://github.com/vim-airline/vim-airline
+" Airline themes https://github.com/vim-airline/vim-airline-themes
+" NERDtree https://github.com/scrooloose/nerdtree
+" Fugitive https://github.com/tpope/vim-fugitive
+
+" Security
+set modelines=0
+
+" Show line numbers
+set number
+
+" Encoding
+set encoding=utf-8
+
+" Whitespace
+set wrap
+set textwidth=79
+set formatoptions=tcqrn1
+set tabstop=8
+set shiftwidth=4
+set softtabstop=0
+set expandtab
+set smarttab
+set noshiftround
+
+" Set colorscheme
+syntax enable
+colorscheme luna-term
+
+" Set runtime path manipulation - vim plugin Pathogen
+execute pathogen#infect()
+
+" Set Airline theme
+let g:airline_theme='powerlineish'
+set laststatus=2
+
+" NERDtree
+autocmd vimenter * NERDTree
+map <C-n> :NERDTreeToggle<CR>
+autocmd StdinReadPre * let s:std_in=1
+autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif
+autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif
---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+apt-get update && apt-get -y upgrade
+
+echo "Installing vim and git packages..."
+apt-get -y install vim git
+
+echo "Installing vim plugins..."
+
+echo "Cloning Pathogen..."
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo "Enhancing JS syntax..."
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
+
+echo "Getting Luna theme..."
+git clone https://github.com/notpratheek/vim-luna.git ~/.vim && mkdir ~/.vim/colors && mv ~/.vim/vim-luna/colors/luna-term.vim ~/.vim/colors
+
+echo "Installing Airline..."
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+vim -u NONE -c "helptags" -c q
+
+echo "...and its themes..."
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+
+echo "Planting NERDtree..."
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+vim -u NONE -c "helptags ~/.vim/bundle/nerdtree/doc" -c q
+
+echo "Capturing Fugitive..."
+git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/fugitive
+vim -u NONE -c "helptags ~/.vim/bundle/fugitive/doc" -c q
+
+echo "Configuring vim..."
+mv ./.vimrc ~/
+
+vim .
+
+
